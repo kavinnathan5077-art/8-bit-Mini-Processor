2b3b4a551e2c8f857d861c0414be77d1966f146c
kavinnathan5077-art <kavinnathan5077@gmail.com>
2026-06-02 16:05:16 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -9,39 +9,40 @@ from cocotb.triggers import ClockCycles
 @cocotb.test()
 async def test_project(dut):
 
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+    # Start clock
+    cocotb.start_soon(Clock(dut.clk, 10, unit="ns").start())
 
+    # Initialize
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
 
+    # Reset
     await ClockCycles(dut.clk, 2)
-
     dut.rst_n.value = 1
 
     # MOVI R0,1
+    # opcode=10 rd=00 imm=01
     dut.ui_in.value = 0b10000001
     await ClockCycles(dut.clk, 1)
 
     # MOVI R1,2
+    # opcode=10 rd=01 imm=10
     dut.ui_in.value = 0b10010010
     await ClockCycles(dut.clk, 1)
 
     # ADD R0,R1
+    # opcode=00 rd=00 rs=01
     dut.ui_in.value = 0b00000100
     await ClockCycles(dut.clk, 1)
 
-    # Wait one cycle
-    dut.ui_in.value = 0
-    await ClockCycles(dut.clk, 1)
-
     # OUT R0
+    # opcode=11 rd=00
     dut.ui_in.value = 0b11000000
     await ClockCycles(dut.clk, 1)
 
-    # Wait for output update
+    # Allow output to update
     await ClockCycles(dut.clk, 1)
 
     result = dut.uo_out.value.to_unsigned()
