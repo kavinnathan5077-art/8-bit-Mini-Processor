b1e39711e9fafcf8699176275cfa5cc836419fd4
kavinnathan5077-art <kavinnathan5077@gmail.com>
2026-06-02 14:42:27 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,33 +8,50 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
     # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+
+    await ClockCycles(dut.clk, 2)
+
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
+    #
+    # MOVI R0,1
+    # opcode=10 rd=00 imm=01
+    #
+    dut.ui_in.value = 0b10000001
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    #
+    # MOVI R1,2
+    # opcode=10 rd=01 imm=10
+    #
+    dut.ui_in.value = 0b10010010
+    await ClockCycles(dut.clk, 1)
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    #
+    # ADD R0,R1
+    # opcode=00 rd=00 rs=01
+    #
+    dut.ui_in.value = 0b00000100
+    await ClockCycles(dut.clk, 1)
+
+    #
+    # OUT R0
+    # opcode=11 rd=00
+    #
+    dut.ui_in.value = 0b11000000
+    await ClockCycles(dut.clk, 1)
+
+    result = dut.uo_out.value.integer
+
+    dut._log.info(f"CPU Output = {result}")
+
+    assert result == 3, f"Expected 3, got {result}"
