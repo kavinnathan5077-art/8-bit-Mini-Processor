8a584a9748e56008cbcc719e8b5707f510943571
kavinnathan5077-art <kavinnathan5077@gmail.com>
2026-06-02 15:24:45 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -12,7 +12,6 @@ async def test_project(dut):
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
@@ -22,37 +21,23 @@ async def test_project(dut):
 
     dut.rst_n.value = 1
 
-    #
     # MOVI R0,1
-    # opcode=10 rd=00 imm=01
-    #
     dut.ui_in.value = 0b10000001
     await ClockCycles(dut.clk, 1)
 
-    #
     # MOVI R1,2
-    # opcode=10 rd=01 imm=10
-    #
     dut.ui_in.value = 0b10010010
     await ClockCycles(dut.clk, 1)
 
-    #
     # ADD R0,R1
-    # opcode=00 rd=00 rs=01
-    #
     dut.ui_in.value = 0b00000100
     await ClockCycles(dut.clk, 1)
 
-    #
     # OUT R0
-    # opcode=11 rd=00
-    #
     dut.ui_in.value = 0b11000000
     await ClockCycles(dut.clk, 1)
 
-  await ClockCycles(dut.clk, 1)
-
-result = dut.uo_out.value.to_unsigned()
+    result = dut.uo_out.value.to_unsigned()
 
     dut._log.info(f"CPU Output = {result}")
 
